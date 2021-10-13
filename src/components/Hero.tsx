ce164d95a6d964464b17587e49acd6c4772d8eae
Subham Bhattacharya <subhambhattacharya700@gmail.com>
2021-10-13 21:33:56 +0530

    init

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="hero-header">
+      <img src="/assets/images/image-hero-desktop.jpg" alt="Hero Header" />
+    </div>
+  );
+};
+
+export default Hero;
